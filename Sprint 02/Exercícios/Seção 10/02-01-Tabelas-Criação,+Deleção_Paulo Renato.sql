6139220c0ab1a9351ff46fad79b8e96284db8a5c
PAULO RENATO BRAGA <paulorenato.jsb@gmail.com>
2024-11-05 01:43:31 -0300

    seção10

---
+++ Sprint 02/Exercícios/Seção 10/02-01-Tabelas-Criação,+Deleção_Paulo Renato.sql
@@ -0,0 +1,46 @@
+-- EXEMPLOS ########################################################################
+
+-- (Exemplo 1) Criação de tabela a partir de uma query
+-- Crie uma tabela chamada customers_age com o id e a idade dos clientes. 
+-- Chame-a de temp_tables.customers_age
+
+select
+	customer_id,
+	datediff('years', birth_date, current_date) idade_cliente
+	into temp_tables.customers_age
+from sales.customers
+
+select *
+from temp_tables.customers_age
+
+-- (Exemplo 2) Criação de tabela a partir do zero
+-- Crie uma tabela com a tradução dos status profissionais dos clientes. 
+-- Chame-a de temp_tables.profissoes
+
+select distinct professional_status
+from sales.customers
+
+create table temp_tables.profissoes (
+	professional_status varchar,
+	status_profissional varchar
+)
+
+insert into temp_tables.profissoes
+(professional_status, status_profissional)
+
+values
+('freelancer', 'freelancer'),
+('retired', 'aposentado(a)'),
+('clt', 'clt'),
+('self_employed', 'autônomo(a)'),
+('other', 'outro'),
+('businessman', 'empresário(a)'),
+('civil_servant', 'funcionário público(a)'),
+('student', 'estudante')
+
+select * from temp_tables.profissoes
+
+-- (Exemplo 3) Deleção de tabelas
+-- Delete a tabela temp_tables.profissoes
+
+drop table temp_tables.profissoes
